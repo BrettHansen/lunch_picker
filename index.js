c290bc8ca8354070d75bae20d844b4ce3fd2ad08
Brett Hansen <hanbrett@amazon.com>
2018-04-30 17:20:12 -0700

    Added better user management (whitelist, uniquness, one vote) and vote shading

--- index.js
+++ index.js
@@ -5,54 +5,99 @@ var fs = require("fs");
 var mime = require("mime");
 var io = require("socket.io")(http);
 var port = 10083;
+var whitelist = require("./whitelist.json");
 
-var data = [
-  {
-    "title" : "Cherry Street Cafe",
-    "votes" : 0
-  },
-  {
-    "title" : "Espresso Vivace",
-    "votes" : 0
-  },
-  {
-    "title" : "Victrola",
-    "votes" : 0
-  },
-  {
-    "title" : "Le Caviste",
-    "votes" : 0
-  }
-];
+var active = {};
+var voters = {};
+
+var data = [];
 
 http.listen(port);
 
 io.on("connection", function(socket) {
-  socket.on("establish", function() {
-    updateClients();
+  socket.on("establish", function(handle, callback) {
+    if(whitelist.indexOf(handle) > -1) {
+      if(is_user_active(handle)) {
+        callback(false);
+        console.log(handle + " rejected; already connected!");
+      } else {
+        callback(true);
+        activate_user(handle, socket.id);
+        console.log(handle + " established connection");
+        updateClients();
+      }
+    } else {
+        callback(false);
+        console.log(handle + " rejected; not in whitelist!");      
+    }
   });
 
   socket.on("suggestion", function(name) {
     data.push({ "title" : name, "votes" : 0 });
+    console.log(active[socket.id] + " suggested " + name);
     updateClients();
   });
 
-  socket.on("vote", function(id) {
-    for(var i = 0; i < data.length; i++) {
-     if(id == data[i].id) {
-      data[i].votes++;
-      updateClients();
-      break;
-     } 
-    }
+  socket.on("vote", function(place_id) {
+    register_user_vote(socket.id, place_id);
+    console.log(active[socket.id] + " voted for " + place_id);
+    updateClients();
+  });
+
+  socket.on("disconnect", function() {
+    console.log(active[socket.id] + " disconnected");
+    deactivate_user(socket.id);
   });
 });
 
+function activate_user(handle, socket_id) {
+  active[socket_id] = handle;
+}
+
+function deactivate_user(socket_id) {
+  delete active[socket_id];
+}
+
+function is_user_active(handle) {
+  for(var socket_id in active)
+    if(active[socket_id] == handle)
+      return true;
+  
+  return false;
+}
+
+function register_user_vote(socket_id, place_id) {
+  if(!(socket_id in active))
+    return;
+
+  var handle = active[socket_id];
+  var decrement_place = "";
+  if(handle in voters)
+    decrement_place = voters[handle];
+
+  var increment_place = place_id;
+
+  for(var i = 0; i < data.length; i++) {
+    if(increment_place == data[i].place_id) {
+      data[i].votes++;
+    }
+    if(decrement_place == data[i].place_id) {
+      data[i].votes--;
+    }
+  }
+
+  voters[handle] = place_id;
+}
+
 function updateClients() {
   for(var i = 0; i < data.length; i++)
-    data[i]["id"] = "element-" + i;
+    if(!("place_id" in data[i]))
+      data[i]["place_id"] = "element-" + i;
 
-  io.emit("update", data);
+  for(var socket_id in active) {
+    var handle = active[socket_id];
+    io.to(socket_id).emit("update", data, voters[handle]);
+  }
 }
 
 
--- main.js
+++ main.js
@@ -1,32 +1,50 @@
 var content_container = $("#content");
 var suggest_box = $("#suggest-box");
 var suggest_button = $("#suggest-button");
+var login_modal = $("#login-modal");
+var handle_input = $("#handle-input");
+var handle_submit = $("#handle-submit");
 var socket = io();
 var restaurants;
 
 function initialize() {
-	socket.on("update", function(data) {
-		updateUI(data);
-	});
-	socket.emit("establish");
+	handle_submit.click(function() {
+		socket.on("update", function(data, vote) {
+			updateUI(data, vote);
+		});
+		socket.emit("establish", handle_input.val(), function(success) {
+			if(success) {
+				suggest_button.click(function() {
+					var name = suggest_box.val();
+					socket.emit("suggestion", name);
+					suggest_box.val("");
+				});
 
-	suggest_button.click(function() {
-		var name = suggest_box.val();
-		socket.emit("suggestion", name);
-		suggest_box.val("");
+				login_modal.modal("hide");
+			} else {
+				handle_input.val("");
+			}
+		});
+
 	});
+
+	login_modal.modal("show");
 }
 
-function updateUI(data) {
+function updateUI(data, vote) {
 	restaurants = data;
 
 	var row = $("<div>", { "class" : "row" });
 
 	for(var i = 0; i < restaurants.length; i++) {
 		var item = restaurants[i];
-		var container = $("<div>", { "id" : item.id, "class" : "vote-card col-md-3" });
+		var container = $("<div>", { "id" : item.place_id, "class" : "vote-card col-md-3" });
 		var title = $("<h3>").text(item.title);
 		var votes = $("<h3>").text(item.votes);
+
+		if(vote == item.place_id) {
+			container.addClass("selected");
+		}
 
 		container.append(title).append(votes);
 		row.append(container);
